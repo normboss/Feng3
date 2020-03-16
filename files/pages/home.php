a48d7cd080f3e8c64b773d8515eea5410de0b18b
DESKTOP\Norm <norm.bosse0@gmail.com>
2020-03-16 11:59:34 -0400

    first commit

---
+++ files/pages/home.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Feng3</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="../css/home.css" media="screen" rel="stylesheet" type="text/css">
+    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+
+    <!-- <link href="https://fonts.googleapis.com/css?family=Oswald:200|Roboto:300i&display=swap" rel="stylesheet"> -->
+    <!-- <link href="https://fonts.googleapis.com/css?family=Oswald:200|Roboto:300i,400i&display=swap" rel="stylesheet"> -->
+    <!-- <link href="https://fonts.googleapis.com/css?family=Oswald:200,400|Roboto:300i,400i&display=swap" rel="stylesheet"> -->
+<body>
+
+    <div class="container">
+
+        <div class="menu">
+            <ul>
+                <li>HOME</li>
+                <li>FENG SHUI</li>
+                <li>ABOUT</li>
+                <li>CONTACT US</li>
+            </ul>
+
+        </div>
+
+
+        <div class="feng-box">
+            <div class="feng-box__upper">
+                Essential
+            </div>
+            <div class="feng-box__lower">
+                <div class="text-upper">
+                FENG
+                </div>
+                <div class="text-lower">
+                SHUI
+                </div>
+            </div>
+        </div>
+
+        <div class="balance">
+                <p>Learn how to live in balance with your universe.
+                    By following a path to an uncluttered lifestyle
+                    we can change your life to one of peaceful
+                    coexistence at home and at work.</p>
+        </div>
+
+        <div class="circle">
+            <div class="circle-top">
+            CHANGE 
+            </div>
+            <div class="circle-top">
+            YOUR LIFE
+            </div>
+            <div class="circle-bottom">
+                24/7
+            </div>
+
+
+        </div>
+
+    </div>
+
+</body>
+
+</html>
